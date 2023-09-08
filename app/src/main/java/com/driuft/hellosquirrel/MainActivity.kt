b514b6fdc205e052fff9298085659807c9a8450c
Zyanik <106106326+geperez8@users.noreply.github.com>
2023-09-07 21:08:15 -0500

    Updated everything except bio

--- app/src/main/java/com/driuft/hellosquirrel/MainActivity.kt
+++ app/src/main/java/com/driuft/hellosquirrel/MainActivity.kt
@@ -35,14 +35,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun configureImage() {
-        profileImage.setImageDrawable(AppCompatResources.getDrawable(this, R.drawable.ic_launcher_foreground))
+        profileImage.setImageDrawable(AppCompatResources.getDrawable(this, R.drawable.avatar_15))
     }
 
     private fun configureName() {
-        profileName.text = getString(
-            R.string.full_name,
-            getString(R.string.first_name),
+        profileName.text =
+            getString(R.string.first_name)+
             getString(R.string.last_name)
-        )
+
     }
 }
